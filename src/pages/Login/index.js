4bb3f76c6de3730bba0af7b4d6a8c0c19c93de18
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-07-19 18:28:19 -0300

    Login screen started: basic layout done

---
+++ src/pages/Login/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import './style.css'
+import logo from '../../common-assets/logo.png'
+function Login(){
+	function handleLogin(event){
+		event.preventDefault()
+
+	}
+	return (
+		<div className="loginContainer">
+			<div className="loginForm">
+				<header>
+					<div className="logoContainer">
+						<Link to="/"><img src={logo} alt="Logo Bluetools"/></Link>
+						<h1>Bluetools</h1>
+					</div>
+				</header>
+				<form action="" onSubmit={handleLogin}>
+					<div className="input-group">
+						<label htmlFor="login">Login</label>
+						<input type="text" id="login"/>
+					</div>
+					<div className="input-group">
+						<label htmlFor="password">Senha</label>
+						<input type="text" id="password"/>
+					</div>
+					<button className="color-primary loginButton">
+						Login
+					</button>
+				</form>
+			</div>
+		</div>
+	)
+}
+
+export default Login
