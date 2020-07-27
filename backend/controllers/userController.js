c737d19492330988bf7a5df0fda10d046c8be4e9
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-07-27 14:27:34 -0300

    Created function to validate passwords

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,7 +19,6 @@ module.exports = {
 			var hash = crypto.createHash('sha256').update(password+user.salt).digest('hex')
 			hash = crypto.createHash('sha256').update(hash+user.salt).digest('hex')
 			hash = crypto.createHash('sha256').update(hash+user.salt).digest('hex')
-
 			if(hash===user.password){
 				return response.json(user)
 			}
@@ -28,10 +27,62 @@ module.exports = {
 			}
 		}
 		else{
-			return response.status(400).json({message: ''})
+			return response.status(400).json({message: 'User not found!'})
 		}
 	},
-	async store(request, response){
+	passwordIsValid(password){
+		if(password.length<8){
+			return false
+		}
+
+		if(!(/^[^ ]+$/g).test(password)){
+			return false
+		}
 		
+		return ((/\w/g).test(password)&&(/[^\w\s]/g).test(password))
+	},
+	async store(request, response){
+		const {
+			id,
+			login,
+			password,
+			salt,
+			name,
+			email,
+			cpf,
+			cnpj,
+			image
+		} = request.body
+
+		if(id){
+			const user = usuario.findByPk(id)
+
+			if(user){
+				if(cpf.replace(/\D/g, '').length!==11&&cpf.replace(/\D/g, '').length!==0){
+					return response.status(400).json({message: `CPF field must have 11 numeric chars or be empty`})
+				}
+
+				if(cnpj.replace(/\D/g, '').length!==14&&cnpj.replace(/\D/g, '').length!==0){
+					return response.status(400).json({message: `CNPJ field must have 14 numeric chars or be empty`})
+				}
+
+				if(!this.passwordIsValid(password)){
+					return response.status(400).json({message: 'Invalid password!'})
+				}
+
+				user.login = login
+				user.name = name
+				user.email = email
+				user.cpf = cpf
+				user.cnpj = cnpj
+				user.image = image
+			}
+			else{
+				return response.status(404).json({message: `Error: user ${id} not found!`})
+			}
+		}
+		else{
+
+		}
 	}
 }
