7890e2fdf94d589531f7d7b85d9da273ba89b6b6
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-07-28 17:27:26 -0300

    Finalized the process to log in on the dashboard panel

---
+++ src/pages/DashBoard/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css'
+import logo from '../../common-assets/logo.png'
+import api from '../../services/api'
+
+function DashBoard(){
+    return (
+        <>
+        </>
+    )
+}
+
+export default DashBoard
