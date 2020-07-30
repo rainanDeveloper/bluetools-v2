625c6eb7e5691abfa19dfdca3606730dd4419a40
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-07-29 22:01:53 -0300

    Added configuration to identify when the application is running on development mode

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,17 @@
 const axios = require('axios')
+const config = require('./config.json')
 
-const api = axios.create({baseURL: 'http://localhost:3333'})
+var baseURL = ''
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // Development:
+    baseURL = config.backendURL
+} else {
+    //Production:
+    baseURL = window.location.origin.toString()
+    
+}
+
+const api = axios.create({baseURL})
 
 export default api
