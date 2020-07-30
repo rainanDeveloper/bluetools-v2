24dccfc29eeb7a9b287798590a70a89a88715e3d
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2020-07-29 21:28:55 -0300

    First build

---
+++ build/precache-manifest.dfe7021596f43c585ab248a323875e14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16becf9da87d1f1511d772f01a3cb55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d228a0ac7c8c02b8e39",
+    "url": "/static/css/main.31650cc6.chunk.css"
+  },
+  {
+    "revision": "d1f9a89412fc7cc2c441",
+    "url": "/static/js/2.8cee6bc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8cee6bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d228a0ac7c8c02b8e39",
+    "url": "/static/js/main.a0833c40.chunk.js"
+  },
+  {
+    "revision": "a2468b4dba51a796a9e5",
+    "url": "/static/js/runtime-main.36994f2c.js"
+  }
+]);
