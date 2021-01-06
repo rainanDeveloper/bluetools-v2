e8fa9ea68d18f3266b407be3a021df99912ff3c5
Rainan Miranda de Jesus <jesus.rainan@gmail.com>
2021-01-06 18:06:42 -0300

    Started contract registration screen

--- src/pages/CadContract/FormCadContract/index.js
+++ src/pages/CadContract/FormCadContract/index.js
@@ -7,10 +7,22 @@ function FormCadContract({contract, saveCallback=((c)=>{})}){
 	const [dueDay, setDueDay] = useState(contract?contract.dueDay:'')
 	const [installationDate, setInstallationDate] = useState(contract?contract.installationDate:'')
 	const [status, setStatus] = useState(contract?contract.status:'')
+	const [customers, setCustomers] = useState([])
 
 	return (
 		<>
-			
+			<form action="" className="formContract">
+				<div className="input-line" id="lineOneContract">
+					<div className="input-group">
+						<label htmlFor="id">Id</label>
+						<input type="text" id value={contract?contract.id:''} disabled/>
+					</div>
+					<div className="input-group">
+						<label htmlFor="customer">Cliente</label>
+						<select name="" id=""></select>
+					</div>
+				</div>
+			</form>
 		</>
 	)
 }
